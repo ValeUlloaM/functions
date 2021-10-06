c9be363c64aa893aaeb63d381ce3e8e10d618456
ValeUlloaM <valeulloam97@gmail.com>
2021-10-06 15:49:40 -0500

    Agrega ejemplo function overloads

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,27 @@ calculador = (num1:number, num2:number, operacion:string) =>  { //Function type
  }
 }
 
+//Function Overloads
+
+function cedula(): number;
+function cedula(quantity: number): number[];
+function cedula (quantity?: number): number | number[]{
+    if(!quantity) return Math.random()
+
+    const arr = []
+    for(let i = 0; i < quantity; i++){
+        arr.push(Math.random())
+    }
+
+    return arr
+}
+
+console.log(cedula());
+console.log(cedula(2));
+
+let number1: number
+number1 = cedula() //Se puede asignar sin problema a la variable gracias al overload
+
 
 
 
