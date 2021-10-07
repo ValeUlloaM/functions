3c28ed1edf2216de9da7d6e465a834d6baf89014
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2021-10-06 21:20:03 -0500

    Incluyo 2 nuevas clases En este commit incluyo 2 nuevas clases con lineas de codigo que van a servir como ejemplo para explicar el tipo object y el tipo unknow de TypeScript

---
+++ src/object_example.ts
@@ -0,0 +1,21 @@
+//Objects, permite crear datos y ajustes para pasarlos dentro del codigo de manera mas eficiente
+let perro = {
+    nombre: 'Phillip',
+    color: 'canela',
+    raza: 'criollo',
+    edad: 4
+};
+
+//Estos pueden ser modificados siempre y cuando se respete el valor de la función
+perro.edad = 5; //number
+perro.nombre = 'Alberto'; //String
+//perro.raza = 6; <-- esto va a marcar un error
+
+
+//Se puede modificar los valores del objeto creado inicialmente
+perro = {
+    nombre: 'Vivente',
+    color: 'naranja',
+    raza: 'Labrador',
+    edad: 2
+}
---
+++ src/unknow_examples.ts
@@ -0,0 +1,27 @@
+//este es un ejemplo de como se implementa el tipo de función "unknow"
+//En este caso va a funcionar en cualquier tipo de dato que se le da, por que se está indicando que el valor es desconocido
+let value: unknown;
+
+value = true;
+value = 12;
+value = "Hola mundo";
+value = [];
+value = {};
+value = Math.random;
+value = null;
+value = undefined;
+value = new TypeError();
+value = Symbol("type");
+
+//Otro ejemplo de la forma en que el unknow va a presentar error
+//En este ejemplo si va a presentar un error por que al value se le está indicando con que tipo de funcion va a trabajar
+let value2: unknown;
+
+let value1: unknown = value2; // funciona
+let value3: any = value2; // funciona
+//let value4: boolean = value2; // No funciona
+//let value5: number = value2; // No funciona
+//let value6: string = value2; // No funciona
+//let value7: object = value2; // No funciona
+//let value8: any[] = value2; // No funciona
+//let value9: Function = value2; // No funciona
